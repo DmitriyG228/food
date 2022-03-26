4118c5dd4a36b11b7e34bf9768c51c8ee0a8a474
Dmitry Grankin <2280905@gmail.com>
2022-03-27 00:54:54 +0300

    d

--- milvus_update.py
+++ milvus_update.py
@@ -7,13 +7,18 @@ from food.psql import *
 import pandas as pd
 from tqdm import tqdm
 import custom_pandas as cpd
-query = f"""select f.*
-        FROM      foods  f
-        WHERE f .clip    is not null
-        """
+query = f"""select    f.*
+            FROM      foods    f
+            LEFT JOIN indexed  i  ON (i.id =       f.id)
+            WHERE f .clip    is not null and
+            i.indexed is null
+            
+            """
 limit = 100000
 for df in tqdm(cpd.read_sql_query(query, engine, chunksize=limit), desc="milvus_update"):
     df = df.rename(columns = {'id':'food_id'})
     df = df[['food_id','clip']]
     collection.insert(df)
-collection.num_entities
+    to_insert = df[['food_id']].rename(columns = {'food_id':'id'})
+    to_insert['indexed'] = True
+    to_insert.to_sql("indexed",engine,index=False,if_exists = 'append',method = 'multi')
