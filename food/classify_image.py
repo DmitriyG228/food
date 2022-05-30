8780fc1457663399d5b563f37fcfc1a579293cbe
DmitriyG228 <2280905@gmail.com>
2022-05-31 00:27:55 +0300

    d

--- food/classify_image.py
+++ food/classify_image.py
@@ -10,7 +10,7 @@ import requests
 from .qdrant import *
 
 # Cell
-foods = read_sql('foods')
+foods = read_sql(table)
 # foods = foods.drop(columns = ['clip'])
 foods = foods.set_index('id')
 
@@ -18,8 +18,9 @@ foods = foods.set_index('id')
 def search_image(url=None,head = 1):
     image_clip = requests.post(f'https://guru.skynet.center/image2vector/?url={url}').json()
     results = client.search(collection_name=collection_name,query_vector=image_clip,top=head)
+    image_clip = torch.Tensor(image_clip)
     df = foods.loc[[r.id for r in results]].copy()
     df['score'] = [r.score for r in results]
     df = df.sort_values('score',ascending=False)
 
-    return df.reset_index()
+    return image_clip,df.reset_index()
